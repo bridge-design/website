3d4a8865165a55d9e5e131421edc83f6ddcef4f0
Kotkoa <kotkoa@gmail.com>
2023-01-06 10:36:52 +0100

    update services page layout with ProfileCards

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,8 +1,12 @@
-import { Text, CtaLink, Testimonial, Advantages } from "@bridge-the-gap/design-system";
+import { Text, CtaLink, Testimonial, Advantages, ProfileCard } from "@bridge-the-gap/design-system";
 import { NextSeo } from "next-seo";
 import Link from "@components/link";
 import ArrowIcon from "../../public/arrow.svg";
 import ArrowLeftIcon from "../../public/arrow-left.svg";
+import PhotoVaria from "../../public/images/meet-the-team/rectangle_60.png";
+import PhotoIrina from "../../public/images/meet-the-team/rectangle_61.png";
+import PhotoSherif from "../../public/images/meet-the-team/rectangle_62.png";
+import PhotoAndriy from "../../public/images/meet-the-team/rectangle_63.png";
 
 export const TestimonialTitle = (
   <Text variant="3Xl" as="h2" className="mt-32">
@@ -31,42 +35,64 @@ const CONTENT = [
   },
 ];
 
+const HOW_IT_WORKS = [
+  {
+    icon: "UserMultiple",
+    title: "Meet the team",
+    description: "Hiring an agency brings additional expertise and knowledge to your business",
+  },
+  {
+    icon: "Cursor_1",
+    title: "Workshop together",
+    description: "Hiring an agency brings additional expertise and knowledge to your business",
+  },
+  {
+    icon: "Network_3Reference",
+    title: "Regular alignement",
+    description: "Hiring an agency brings additional expertise and knowledge to your business",
+  },
+  {
+    icon: "CircleDash",
+    title: "Iterations",
+    description: "Hiring an agency brings additional expertise and knowledge to your business",
+  },
+  {
+    icon: "Task",
+    title: "Agile",
+    description: "Hiring an agency brings additional expertise and knowledge to your business",
+  },
+];
+
 export const testimonials = [
   {
-    name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
-    text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.",
+    name: "Nico Peham - Tech Lead at pcode",
+    avatar: "",
+    text: "Building a Design System 🛠️ Our members Marlene Höglinger and Nico Peham participated in the Hands-on with Design Systems workshop by Varya Stepanova and Sherif Saleh last week. It was about building design systems with Figma and React and gaining in-depth hands-on experience. It was especially exciting to see how other companies build their design systems and to exchange experiences.",
     colors: "bg-primary-100 text-light-on-background-900",
   },
   {
-    name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
-    text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.",
+    name: "David Teissier - Directeur Général Publicom",
+    avatar: "",
+    text: "Sherif was a true professional in his business. Great skills in web design and UX. CSS has no secrets for him also. We worked together on web projects to large sizes.",
     colors: "bg-primary-500 text-light-on-background-100",
   },
   {
     name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
+    avatar: "",
     text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.",
     colors: "bg-light-background-50 text-light-on-background-100",
   },
   {
-    name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
-    text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.",
+    name: "Anthony Tahar - Webmarketing Master @ MEDISUR SAS",
+    avatar: "",
+    text: "Sherif is a proactive and tireless contributor who would make a great addition to any team. He completed his assignments on time and never failed to add a creative touch when appropriate.",
     colors: "bg-primary-100 text-light-on-background-900",
   },
   {
-    name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
-    text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.",
+    name: "Alexander Petrovskiy",
+    avatar: "",
+    text: "Varya is a highly skilled professional designer and developer. Along with these qualities, she is a great leader, a team player, and a business partner! She is a valuable asset to any team when it comes to solving complex business problems! I'm glad to recommend her!",
     colors: "bg-primary-500 text-light-on-background-100",
-  },
-  {
-    name: "Junaid Rasheed",
-    avatar: "/sampleImages/rasheed_avatar.png",
-    text: "Varya is an expert in Design System domain. I have had chance to work with her in past and I",
-    colors: "bg-light-background-50 text-light-on-background-50",
   },
 ];
 
@@ -112,93 +138,124 @@ export default function Services() {
       <Text as="h2" variant="3xl" className="mt-32 animate-fadein animate-reveal">
         Design Sytem processes
       </Text>
-      <div className="relative w-full h-64">
-        <div className="top-0 right-0 z-10 w-full ">
-          <div className="flex items-center gap-[4%] mt-20 ">
-            <Text as="p" variant="3xl" className="">
-              Setup
-            </Text>
-            <ArrowIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-            <Text as="p" variant="3xl" className="">
-              POC
-            </Text>
-            <ArrowIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-            <Text as="p" variant="3xl" className="">
-              Outline
-            </Text>
-            <ArrowIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-            <Text as="p" variant="3xl" className="">
-              Alignment
-            </Text>
-            <ArrowIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-          </div>
-          <div className="flex gap-24 mt-20 ml-32">
-            <Text as="p" variant="3xl" className="">
-              Handover
-            </Text>
-            <ArrowLeftIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-            <Text as="p" variant="3xl" className="">
-              Adoption
-            </Text>
-            <ArrowLeftIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-            <Text as="p" variant="3xl" className="">
-              Components library
-            </Text>
-            <ArrowLeftIcon
-              width="116"
-              height="16"
-              fill="currentColor"
-              className="text-light-on-background-900"
-              title="arrow"
-            />
-          </div>
+      <div className="relative w-full h-54">
+        <div className="flex items-baseline gap-[3%] mt-20">
+          <Text as="p" variant="3xl" className="">
+            Setup
+          </Text>
+          <ArrowIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+          <Text as="p" variant="3xl" className="">
+            POC
+          </Text>
+          <ArrowIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+          <Text as="p" variant="3xl" className="">
+            Outline
+          </Text>
+          <ArrowIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+          <Text as="p" variant="3xl" className="">
+            Alignment
+          </Text>
+          <ArrowIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+        </div>
+        <div className="flex items-baseline gap-[3%] mt-20 ml-24">
+          <Text as="p" variant="3xl" className="">
+            Handover
+          </Text>
+          <ArrowLeftIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+          <Text as="p" variant="3xl" className="">
+            Adoption
+          </Text>
+          <ArrowLeftIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
+          <Text as="p" variant="3xl" className="min-w-max">
+            Components library
+          </Text>
+          <ArrowLeftIcon
+            width="116"
+            height="16"
+            fill="currentColor"
+            className="text-light-on-background-900"
+            title="arrow"
+          />
         </div>
       </div>
       <Text as="h2" variant="3xl" className="mt-32 mb-10 animate-fadein animate-reveal">
         How it works
       </Text>
-      <Advantages colorSchema="light" content={CONTENT} />
+      <Advantages colorSchema="light" content={HOW_IT_WORKS} />
       <Testimonial testimonials={testimonials} title={TestimonialTitle} />
       <Text as="h2" variant="3xl" className="mt-32 animate-fadein animate-reveal">
         Meet the team
       </Text>
-      <div></div>
+      <div className="grid grid-cols-2 gap-10">
+        <ProfileCard
+          photoUrl={PhotoVaria}
+          name="Varya Stepanova"
+          role="Design systems architect and engineering manager"
+          twitter="#"
+          linkedin="#"
+          github="#"
+        />
+        <ProfileCard
+          photoUrl={PhotoIrina}
+          name="Irina (Samoylova) Illustrova"
+          role="Senior Frontend Engineer"
+          twitter="#"
+          linkedin="#"
+          github="#"
+        />
+        <ProfileCard
+          photoUrl={PhotoSherif}
+          name="Sherif Saleh"
+          role="Design manager"
+          twitter="#"
+          linkedin="#"
+          github="#"
+        />
+        <ProfileCard
+          photoUrl={PhotoAndriy}
+          name="Andriy Kotko"
+          role="Senior Frontend Engineer"
+          twitter="#"
+          linkedin="#"
+          github="#"
+        />
+      </div>
     </>
   );
 }
