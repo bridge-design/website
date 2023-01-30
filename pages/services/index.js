b80e69c58915cc7aa914d92cde0082c1ad1d629a
Kotkoa <kotkoa@gmail.com>
2023-01-30 11:13:16 +0100

    hide hands-on-workshop

--- pages/services/index.js
+++ pages/services/index.js
@@ -145,7 +145,7 @@ export default function Services() {
       <Text as="h2" variant="4xl" className="mt-32 mb-10 animate-fadein animate-reveal">
         How it works
       </Text>
-      <Advantages colorSchema="light" content={HOW_IT_WORKS} />
+      <Advantages content={HOW_IT_WORKS} />
       <HelpTeams className="mt-24" photoUrl="/images/meet-the-team/rectangle_14.png" />
       <Testimonial testimonials={TESTIMONIALS} title={TestimonialTitle} />
       <MeatTheTeam />
