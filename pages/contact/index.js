d5b6aabcc92c67e0a5ebb27ef23a4433113218ec
Kotkoa <kotkoa@gmail.com>
2023-03-06 09:50:19 +0100

    add contact page with email

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,14 @@
+import { Text, CtaLink } from "@bridge-the-gap/design-system";
+import Link from "@components/link";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="text-center">
+      <CtaLink href="mailto:mail@bridge-the-gap.dev" target="_blank" as={Link} underline>
+        mail@bridge-the-gap.dev
+      </CtaLink>
+    </div>
+  );
 };
 
 export default Contact;
