8e11a30e136b930542b1a5a250d720a61657ee18
Varya Stepanova <mail@varya.me>
2024-09-28 11:07:57 +0300

    logo component

--- .storybook/main.js
+++ .storybook/main.js
@@ -22,5 +22,21 @@ const config = {
   staticDirs: ['../public'],
 
   docs: {},
+
+  webpackFinal: async (config, { configType }) => {
+    // Find the existing rule for handling files
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'))
+
+    // Exclude SVGs from the default file-loader so that they can be handled by @svgr/webpack
+    fileLoaderRule.exclude = /\.svg$/
+
+    // Add @svgr/webpack loader to handle SVG imports as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 export default config
