b7fa732c5e3d2ad76646943cf46140f820c32c10
Varya Stepanova <mail@varya.me>
2024-09-29 22:42:40 +0300

    gradients

--- components-new/Section/Section.tsx
+++ components-new/Section/Section.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import classnames from 'classnames'
 
 interface SectionProps {
   children: React.ReactNode
   backgroundColor?: string
+  className?: string
 }
 
-export const Section: React.FC<SectionProps> = ({ children, backgroundColor = '' }) => {
+export const Section: React.FC<SectionProps> = ({ children, backgroundColor = '', className }) => {
   return (
     <section
-      className="w-full"
+      className={classnames('w-full', className)}
       style={{ backgroundColor: `var(${backgroundColor})` }} // Using the CSS variable for background color
     >
       <div className="mx-auto min-h-screen w-full max-w-[1280px] sm:min-w-full lg:min-w-[1024px]">
