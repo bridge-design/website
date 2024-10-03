e18f69f0abab436db14132a1f47496473537767d
Varya Stepanova <mail@varya.me>
2024-10-04 00:36:19 +0300

    codestyle

--- tokens/typography.stories.tsx
+++ tokens/typography.stories.tsx
@@ -75,21 +75,21 @@ console.log(hello);`}</code>
 export const BrandColors: Story = {
   render: () => (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Brand Colors</h2>
+      <h2 className="font-bold mb-4 text-2xl">Brand Colors</h2>
 
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-brand-dark)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-brand-dark)]">
         Brand Dark Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-brand-green)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-brand-green)]">
         Brand Green Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-brand-pink)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-brand-pink)]">
         Brand Pink Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-brand-yellow)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-brand-yellow)]">
         Brand Yellow Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-brand-light)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-brand-light)]">
         Brand Light Text
       </div>
     </div>
@@ -99,15 +99,15 @@ export const BrandColors: Story = {
 export const NeutralColors: Story = {
   render: () => (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Neutral Colors</h2>
+      <h2 className="font-bold mb-4 text-2xl">Neutral Colors</h2>
 
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-neutral-100)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-neutral-100)]">
         Neutral 100 Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-neutral-600)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-neutral-600)]">
         Neutral 600 Text
       </div>
-      <div className="text-5xl font-bold mb-4 text-[var(--btg-color-neutral-1000)]">
+      <div className="font-bold mb-4 text-5xl text-[var(--btg-color-neutral-1000)]">
         Neutral 1000 Text
       </div>
     </div>
@@ -117,13 +117,13 @@ export const NeutralColors: Story = {
 export const GradientTexts: Story = {
   render: () => (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Gradient Texts</h2>
+      <h2 className="font-bold mb-4 text-2xl">Gradient Texts</h2>
 
-      <div className="text-gradient-green text-6xl font-bold mb-4">
+      <div className="text-gradient-green font-bold mb-4 text-6xl">
         Gradient Green to Light Text
       </div>
-      <div className="text-gradient-pink text-6xl font-bold mb-4">Gradient Pink to Light Text</div>
-      <div className="text-gradient-yellow text-6xl font-bold mb-4">
+      <div className="text-gradient-pink font-bold mb-4 text-6xl">Gradient Pink to Light Text</div>
+      <div className="text-gradient-yellow font-bold mb-4 text-6xl">
         Gradient Yellow to Light Text
       </div>
     </div>
