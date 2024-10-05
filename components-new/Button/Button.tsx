0dcc36c4b20465edd705360ec239b847e271a580
Irina Illustrova <illustrova@gmail.com>
2024-10-05 22:15:28 +0300

    fix(Button): update stories and refactor

--- components-new/Button/Button.tsx
+++ components-new/Button/Button.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import { useMemo } from 'react'
 
-interface ButtonProps {
+export const buttonVariants = ['primary', 'secondary', 'ghost'] as const
+export interface ButtonProps {
   children: React.ReactNode
-  variant?: 'primary' | 'secondary'
+  /** Button variant */
+  variant?: (typeof buttonVariants)[number]
   onClick?: () => void
 }
 
 export const Button: React.FC<ButtonProps> = ({ children, variant = 'secondary', onClick }) => {
-  let baseStyles = `px-4 py-2 rounded transition-colors duration-200 ease-in-out font-semibold`
-  let variantStyles = ''
+  const baseStyles = `px-4 py-2 rounded transition-colors duration-200 ease-in-out font-semibold`
+  const focusStyles = 'focus:outline-none focus:ring focus:ring-violet-300'
 
-  switch (variant) {
-    case 'primary':
-      variantStyles = `bg-[var(--btg-color-primary-500)] text-white hover:bg-[var(--btg-color-primary-400)]`
-      break
-    case 'secondary':
-    default:
-      variantStyles = `bg-[var(--btg-color-neutral-600)] text-[var(--btg-color-neutral-200)] hover:bg-[var(--btg-color-neutral-500)]`
-      break
-  }
+  const variantStyles = useMemo(() => {
+    switch (variant) {
+      case 'primary':
+        return `bg-[var(--btg-color-primary-500)] text-white hover:bg-[var(--btg-color-primary-400)]`
+      case 'ghost':
+        return `text-[var(--btg-color-primary-500)] hover:text-[var(--btg-color-primary-400)]`
+
+      case 'secondary':
+      default:
+        return `bg-[var(--btg-color-neutral-600)] text-[var(--btg-color-neutral-200)] hover:bg-[var(--btg-color-neutral-500)]`
+    }
+  }, [variant])
 
   return (
-    <button className={`${baseStyles} ${variantStyles}`} onClick={onClick}>
+    <button className={`${baseStyles} ${variantStyles} ${focusStyles}`} onClick={onClick}>
       {children}
     </button>
   )
