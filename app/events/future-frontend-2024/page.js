a5100ce6249fb6fc2711b7efb050aba59e02a216
Varya Stepanova <mail@varya.me>
2024-10-07 19:21:03 +0300

    dark mode

--- app/events/future-frontend-2024/page.js
+++ app/events/future-frontend-2024/page.js
@@ -26,7 +26,8 @@ function SectionTitle({ className, iconClassName, children }) {
 export default function FutureFrontendEvent() {
   return (
     <>
-      <div className="leading-normal tracking-normal text-center text-light-on-background-900">
+      {/* Page Content */}
+      <div className="leading-normal tracking-normal text-center text-light-on-background-900 dark:text-light-on-background-100">
         <Text as="p" variant="xlMedium" className="mt-24">
           <Emoji symbol="👋 " label="hand" />
           talk materials
@@ -37,10 +38,11 @@ export default function FutureFrontendEvent() {
         <Text as="h1" variant="6Xl" className="mx-auto mt-4 max-w-3xl">
           Enhancing Design Systems via Seamless Collaboration
         </Text>
+
+        {/* Cards Section */}
         <div className="mt-24 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3">
           <Card
-            className="min-h-[240px]"
-            key="september"
+            className="min-h-[240px] bg-white dark:bg-gray-800 dark:text-light-on-background-100"
             heading="ORIGINAL SLIDES"
             link={{
               text: 'Slides in PDF',
@@ -49,15 +51,16 @@ export default function FutureFrontendEvent() {
             linkComponent={Link}
           >
             <div className="mb-8">
-              <Text variant="smMedium" className="mb-3">
+              <Text variant="smMedium" className="mb-3 dark:text-light-on-background-300">
                 Evolution of workflows, transforming a design system into a real product by
                 enhancing communication with high-level stakeholders, engaging peers, and
                 streamlining collaboration.
               </Text>
             </div>
           </Card>
+
           <Card
-            className="min-h-[240px]"
+            className="min-h-[240px] bg-white dark:bg-gray-800 dark:text-light-on-background-100"
             heading="DIRECTOR'S CUT"
             link={{
               text: `Even more slides`,
@@ -66,14 +69,15 @@ export default function FutureFrontendEvent() {
             linkComponent={Link}
           >
             <div className="mb-8">
-              <Text variant="smMedium" className="mb-3">
+              <Text variant="smMedium" className="mb-3 dark:text-light-on-background-300">
                 More detailed insights into engaging peers, streamlining collaboration and working
                 with the in-house design and development community.
               </Text>
             </div>
           </Card>
+
           <Card
-            className="min-h-[240px]"
+            className="min-h-[240px] bg-white dark:bg-gray-800 dark:text-light-on-background-100"
             heading="COMPLETE DESIGN SYSTEM"
             link={{
               text: `Miro Board`,
@@ -82,7 +86,7 @@ export default function FutureFrontendEvent() {
             linkComponent={Link}
           >
             <div className="mb-8">
-              <Text variant="smMedium" className="mb-3">
+              <Text variant="smMedium" className="mb-3 dark:text-light-on-background-300">
                 Compilation of existing design systems, highlighting their features and linking
                 examples.
               </Text>
@@ -91,7 +95,8 @@ export default function FutureFrontendEvent() {
         </div>
       </div>
 
-      <div className="mx-auto mt-28 flex max-w-4xl flex-col md:flex-row">
+      {/* Event Description Section */}
+      <div className="mx-auto mt-28 flex max-w-4xl flex-col text-light-on-background-900 dark:text-light-on-background-100 md:flex-row">
         <SectionTitle
           icon="Education32"
           className="w-full flex-shrink-0 justify-start md:w-2/5 md:flex-col"
@@ -99,12 +104,7 @@ export default function FutureFrontendEvent() {
         >
           More about design systems?
         </SectionTitle>
-        {/* <div className="flex items-start justify-start flex-shrink-0 w-full md:flex-col md:w-2/5">
-        <Icon name="Education32" className="pr-4 mb-4 mr-6 md:mr-0" />
-        <Text variant="6Xl" as="h2">
-          What you <span className="md:block">will learn?</span>
-        </Text>
-      </div> */}
+
         <Text variant="xlMedium" as="div" className="mt-16 md:mt-0">
           <p className="mb-6">
             Missing those informal chats about design systems? So are we! Join us for a cozy remote
@@ -116,12 +116,14 @@ export default function FutureFrontendEvent() {
           <p></p>
           <Link
             href="https://www.linkedin.com/events/casualbreakfastfordesignsystems7249054983411691520/"
-            className="text-blue-600 hover:underline"
+            className="text-blue-600 hover:underline dark:text-blue-400"
           >
             Date: October 30th, 2023 | Time: 9:30 CET (8:30 GMT / 10:30 EET) @ your laptop
           </Link>
 
-          <h3 className="font-bold mb-4 mt-8 text-3Xl">What to Expect?</h3>
+          <h3 className="font-bold mb-4 mt-8 text-3Xl dark:text-light-on-background-100">
+            What to Expect?
+          </h3>
           <ul className="list-inside list-disc">
             <li className="mb-3">
               A relaxed atmosphere where you can brew your morning coffee during the event
@@ -129,7 +131,10 @@ export default function FutureFrontendEvent() {
             <li className="mb-3">Presentation on "How to build a strategy, not a roadmap"</li>
             <li className="mb-3">Open discussions for sharing ideas and experiences</li>
           </ul>
-          <h3 className="font-bold mb-4 mt-8 text-3Xl">Why Join?</h3>
+
+          <h3 className="font-bold mb-4 mt-8 text-3Xl dark:text-light-on-background-100">
+            Why Join?
+          </h3>
           <p>
             This breakfast is for connecting with the global design system community in a casual,
             fun way. Whether you're experienced or new to design systems, it's the perfect space for
