d6e2cee76e68880dbdd3c101f64916849e692de1
Varya Stepanova <mail@varya.me>
2024-10-24 21:29:24 +0300

    Blob component

---
+++ components-new/Blob/Blob.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Blob } from './Blob'
+
+const meta: Meta<typeof Blob> = {
+  title: 'Components/Blob',
+  component: Blob,
+  argTypes: {
+    color: {
+      control: 'color',
+      description: 'Color of the blob.',
+      defaultValue: '#4a90e2',
+    },
+    size: {
+      control: 'number',
+      description: 'Size of the blob (width and height).',
+      defaultValue: 300,
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Blob>
+
+export const Default: Story = {
+  args: {
+    color: '#4a90e2',
+    size: 300,
+  },
+}
+
+export const CustomSize: Story = {
+  args: {
+    color: '#ff6347',
+    size: 500,
+  },
+}
