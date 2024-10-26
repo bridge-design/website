1fcb8364588471ce3782e16cb42dee7e8c069a70
Varya Stepanova <mail@varya.me>
2024-10-26 23:54:49 +0300

    Expertise section

--- pages/Home.stories.tsx
+++ pages/Home.stories.tsx
@@ -43,16 +43,17 @@ export const Default: Story = {
           </a>
         </div>
       </Hero>
-      <Section>
-        <h2 className="mb-6 text-center text-3xl">Our Services</h2>
+      <Section className="pb-12">
+        <h2 className="mb-6 text-center text-4xl">Our Design Systems Expertise</h2>
         <ThreeColumnLayout>
           <ThreeColumnLayout.Column>
             <h3 className="font-bold flex items-center space-x-2 text-2xl">
               <Blob animate={true} size={100} color="var(--btg-color-accent-500)" />
               <span>Audit & Strategy</span>
             </h3>
-            <p className="prose mb-6 px-3">
-              This is a default card with a heading, text content, and a link.
+            <p className="prose mb-6 px-3 text-lg">
+              We assess your current workflows, libraries, and team setups to analyse and craft
+              strategy and roadmap to achive your next goals
             </p>
           </ThreeColumnLayout.Column>
 
@@ -61,8 +62,9 @@ export const Default: Story = {
               <Blob animate={true} size={100} variant="2" color="var(--btg-color-primary-500)" />
               <span>Implementation</span>
             </h3>
-            <p className="prose mb-6 px-3">
-              This is a default card with a heading, text content, and a link.
+            <p className="prose mb-6 px-3 text-lg">
+              We collaborate with your team to build scalable components and ensure seamless
+              integration with your existing technology stack
             </p>
           </ThreeColumnLayout.Column>
 
@@ -71,18 +73,20 @@ export const Default: Story = {
               <Blob animate={true} size={100} variant="3" color="var(--btg-color-highlight-700)" />
               <span>Architecture</span>
             </h3>
-            <p className="prose mb-6 px-3">
-              This is a default card with a heading, text content, and a link.
+            <p className="prose mb-6 px-3 text-lg">
+              Drawing from our extensive experience with diverse design systems, we find flexible
+              and scalable solutions for your architecture.
             </p>
           </ThreeColumnLayout.Column>
 
           <ThreeColumnLayout.Column>
             <h3 className="font-bold flex items-center space-x-2 text-2xl">
               <Blob animate={true} size={100} variant="4" color="var(--btg-color-highlight-1000)" />
-              <span>Streamline</span>
+              <span>Workflows</span>
             </h3>
-            <p className="prose mb-6 px-3">
-              Streamlined workflows to enhance team collaboration and productivity.
+            <p className="prose mb-6 px-3 text-lg">
+              We ensure efficient workflows with design&development streamline, automated testing,
+              tuned CI, and data-based decision making.
             </p>
           </ThreeColumnLayout.Column>
 
@@ -91,7 +95,10 @@ export const Default: Story = {
               <Blob animate={true} size={100} variant="5" color="var(--btg-color-accent-800)" />
               <span>Business value</span>
             </h3>
-            <p className="prose mb-6 px-3">Data driven management etc</p>
+            <p className="prose mb-6 px-3 text-lg">
+              We transform design systems into measurable assets to ensure that their impact is
+              aligned with core business objectives.
+            </p>
           </ThreeColumnLayout.Column>
 
           <ThreeColumnLayout.Column>
@@ -99,23 +106,12 @@ export const Default: Story = {
               <Blob animate={true} size={100} variant="6" color="var(--btg-color-primary-800)" />
               <span>Trainings</span>
             </h3>
-            <p className="prose mb-6 px-3">
-              This is a default card with a heading, text content, and a link.
+            <p className="prose mb-6 px-3 text-lg">
+              Our workshops and tranings cover core principles, component building, and governance
+              to empower teams for long-term success and autonomy.
             </p>
           </ThreeColumnLayout.Column>
         </ThreeColumnLayout>
-
-        <p>
-          <b>Our Expertise</b>
-        </p>
-        <p>
-          - Design System Implementation Brief description: "We build scalable design systems that
-          unify your brand and accelerate development." - Technical Consulting Brief description:
-          "Expert advice on integrating design and development processes for optimal efficiency." -
-          Process Optimization Brief description: "Streamlining workflows to enhance team
-          collaboration and productivity." - Training & Workshops Brief description: "Empowering
-          your team with the knowledge and tools for sustained success."
-        </p>
       </Section>
       <Section backgroundColor="--btg-color-highlight-700">
         <p>
