7929507567b6d0bc8d144d9bf6c0ec1c57a396b3
Varya Stepanova <mail@varya.me>
2024-10-29 20:05:27 +0200

    Testimonials: brushed code

--- components-new/Testimonial/Testimonial.stories.tsx
+++ components-new/Testimonial/Testimonial.stories.tsx
@@ -23,37 +23,37 @@ const testimonials = [
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.',
-    colors: 'bg-primary-100 text-light-on-background-900',
+    colors: 'bg-[var(--btg-color-accent-800)] text-[var(--btg-color-neutral-100)]',
   },
   {
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.',
-    colors: 'bg-primary-500 text-light-on-background-100',
+    colors: 'bg-[var(--btg-color-accent-500)] text-[var(--btg-color-neutral-1000)]',
   },
   {
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.',
-    colors: 'bg-light-background-50 text-light-on-background-100',
+    colors: 'bg-[var(--btg-color-accent-300)] text-[var(--btg-color-neutral-1000)]',
   },
   {
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.',
-    colors: 'bg-primary-100 text-light-on-background-900',
+    colors: 'bg-[var(--btg-color-accent-800)] text-[var(--btg-color-neutral-100)]',
   },
   {
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I found her very intellectual and hardworking individual. She has sounds technical background and deep understanding of web applications development. She leans into solving a problem not just defining it well. I would gladly recommend Varya to anyone who is looking for a Design System expert.',
-    colors: 'bg-primary-500 text-light-on-background-100',
+    colors: 'bg-[var(--btg-color-accent-500)] text-[var(--btg-color-neutral-1000)]',
   },
   {
     name: 'Junaid Rasheed',
     avatar: '/sampleImages/rasheed_avatar.png',
     text: 'Varya is an expert in Design System domain. I have had chance to work with her in past and I',
-    colors: 'bg-light-background-50 text-light-on-background-50',
+    colors: 'bg-[var(--btg-color-accent-300)] text-[var(--btg-color-neutral-1000)]',
   },
 ]
 
@@ -61,40 +61,7 @@ type Story = StoryObj<typeof Testimonial>
 
 export const Default: Story = {
   args: {
-    title: (
-      <h2 className="font-bold text-3xl text-[var(--btg-color-primary-500)]">
-        What Our Clients Say
-      </h2>
-    ),
+    title: <h2 className="text-3xl">People Say</h2>,
     testimonials: testimonials,
-  },
-}
-
-export const CustomColors: Story = {
-  args: {
-    title: (
-      <h2 className="font-bold text-3xl text-[var(--btg-color-accent-500)]">Client Testimonials</h2>
-    ),
-    testimonials: [
-      {
-        name: 'Alice Johnson',
-        text: 'Wonderful experience, truly professional service.',
-        avatar: '/path-to-avatar/alicejohnson.jpg',
-        colors: 'bg-[var(--btg-color-accent-50)] text-[var(--btg-color-accent-500)]',
-      },
-      {
-        name: 'Bob Martin',
-        text: 'Top-notch from start to finish!',
-        avatar: '/path-to-avatar/bobmartin.jpg',
-        colors: 'bg-[var(--btg-color-grey-50)] text-[var(--btg-color-grey-500)]',
-      },
-    ],
-  },
-}
-
-export const WithTitleOnly: Story = {
-  args: {
-    title: <h2 className="font-bold text-3xl text-[var(--btg-color-primary-700)]">Feedback</h2>,
-    testimonials: [],
   },
 }
