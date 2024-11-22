619da8b3402d490798d53a68a4705e4a1033aff9
Varya Stepanova <mail@varya.me>
2024-11-22 20:53:27 +0100

    LogosCloud component

---
+++ components-new/LogosCloud/LogosCloud.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface Logo {
+  src: string
+  alt: string
+  link: string
+}
+
+interface LogosCloudProps {
+  logos: Logo[]
+}
+
+export const LogosCloud: React.FC<LogosCloudProps> = ({ logos }) => {
+  return (
+    <div className="flex flex-wrap justify-center gap-12">
+      {logos.map((logo, index) => (
+        <a
+          href={logo.link}
+          key={index}
+          className="block transition-opacity hover:opacity-80"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo.src} alt={logo.alt} className="h-16 w-auto" />
+        </a>
+      ))}
+    </div>
+  )
+}
