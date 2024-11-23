bc3e3f26f44b61cd575f9fcfe8fd4320d7708907
Varya Stepanova <mail@varya.me>
2024-11-23 23:06:52 +0100

    clean tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 // @ts-check
-const { fontFamily } = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
-const designSystem = require('@bridge-the-gap/design-system')
-const { merge } = require('lodash')
-const bridgeConfig = designSystem.twconfig
 
 /** @type {import("tailwindcss/types").Config } */
 const config = {
@@ -17,74 +12,12 @@ const config = {
     './node_modules/\\@bridge-the-gap/design-system/dist/**/*.js',
     './node_modules/\\@bridge-the-gap/design-system/dist/*.js',
   ],
-  darkMode: 'class',
   theme: {
-    extend: {
-      lineHeight: {
-        11: '2.75rem',
-        12: '3rem',
-        13: '3.25rem',
-        14: '3.5rem',
-      },
-      fontFamily: {
-        sans: ['var(--font-inter)', ...fontFamily.sans],
-      },
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.600')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2': {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-            },
-            h3: {
-              fontWeight: '600',
-            },
-            code: {
-              color: theme('colors.indigo.500'),
-            },
-          },
-        },
-        invert: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.400')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2,h3,h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
-          },
-        },
-      }),
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1.5rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [],
 }
 
-let finalConfig = merge({}, bridgeConfig, config)
-
-// Ensure that finalConfig.content.patterns is an array before adding to it
-finalConfig.content.safelist = finalConfig.content.safelist || []
-finalConfig.content.safelist.push('bg-6XL')
+let finalConfig = config
 
 module.exports = finalConfig
