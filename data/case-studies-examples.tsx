d60f5a9dda80cf6486b4f64f2a4912471560318e
Varya Stepanova <mail@varya.me>
2024-11-23 10:49:22 +0100

    Case Studies carousel

--- data/case-studies-examples.tsx
+++ data/case-studies-examples.tsx
@@ -27,4 +27,32 @@ export const caseStudiesExamples = [
     slug: 'case-study-5',
     logo: { src: '/logo/elisa-logo.svg', alt: 'Logo 5' },
   },
+  {
+    title: 'Case Study 1',
+    slug: 'case-study-1',
+    logo: { src: '/logo/appomni-logo.svg', alt: 'Logo 1' },
+    description: `  Renewal of the design system at Elisa took place in the years 2020 and 2021. The
+  the project included complete re-design and re-coding of 35 components and solid foundational
+  work in design, development, infrastructure, and community managing areas.`,
+  },
+  {
+    title: 'Case Study 2',
+    slug: 'case-study-2',
+    logo: { src: '/logo/yara-logo.svg', alt: 'Logo 2' },
+  },
+  {
+    title: 'Case Study 3',
+    slug: 'case-study-3',
+    logo: { src: '/logo/f-secure-logo.svg', alt: 'Logo 3' },
+  },
+  {
+    title: 'Case Study 4',
+    slug: 'case-study-4',
+    logo: { src: '/logo/metro-logo.svg', alt: 'Logo 4' },
+  },
+  {
+    title: 'Case Study 5',
+    slug: 'case-study-5',
+    logo: { src: '/logo/elisa-logo.svg', alt: 'Logo 5' },
+  },
 ]
