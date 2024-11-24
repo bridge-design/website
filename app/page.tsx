b0533baef67e67f06e37a062bf673888d85c0b2c
Varya Stepanova <mail@varya.me>
2024-11-24 09:20:56 +0100

    Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import { CtaLink, Text } from '@bridge-the-gap/design-system'
-
-import Link from '@/components/Link'
+import { Hero, CtaLink } from '@/components-new/index.tsx'
 
 export default async function Page() {
   return (
-    <div className="text-center">
-      <Text as="h1" variant="7Xl" className="mx-auto mb-[14%] max-w-5xl">
-        We team up with designers and developers to build and scale design systems.
-      </Text>
-      <CtaLink
-        href="/contact"
-        as={Link}
-        // onClick={() => (window.location = "mailto:mail@bridge-the-gap.dev")}
-        underline
-        className="animate-fadeindelay"
-      >
-        Start a conversation
-      </CtaLink>
-    </div>
+    <>
+      <Hero backgroundColor="var(--btg-hero-background-teal)">
+        <div className="text-center lg:mt-8">
+          <h1 className="font-6xl mx-auto mb-16 text-4xl leading-hero lg:w-4/5 lg:text-6xl">
+            We team up with designers and developers to build and scale design systems
+          </h1>
+          <CtaLink
+            to="/contact"
+            underline={true}
+            className="text-xl"
+            borderClassName="border-[var(--btg-hero-text-color)] hover:border-[var(--btg-hero-text-color)]"
+          >
+            Start a conversation
+          </CtaLink>
+        </div>
+      </Hero>
+    </>
   )
 }
