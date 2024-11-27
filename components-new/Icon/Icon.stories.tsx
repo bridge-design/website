c379021a0ba3f9379b973f992cdaba90e25ed3a9
Varya Stepanova <mail@varya.me>
2024-11-27 18:42:08 +0100

    Advantages and Icon components

---
+++ components-new/Icon/Icon.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Icon } from './Icon'
+
+const meta: Meta<typeof Icon> = {
+  title: 'Components/Icon',
+  component: Icon,
+  tags: ['draft'],
+  argTypes: {
+    name: {
+      control: 'text',
+      description: 'Name of the Carbon Design icon to render.',
+      defaultValue: 'UserIdentification',
+    },
+    className: {
+      control: 'text',
+      description: 'Additional CSS classes for the icon container.',
+    },
+    size: {
+      control: { type: 'number', min: 16, max: 64, step: 1 },
+      description: 'Size of the icon.',
+      defaultValue: 32,
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Icon>
+
+export const Default: Story = {
+  args: {
+    name: 'UserIdentification',
+  },
+}
+
+export const CustomSize: Story = {
+  args: {
+    name: 'Rocket',
+    size: 48,
+  },
+}
+
+export const CustomStyle: Story = {
+  args: {
+    name: 'TreeView',
+    size: 32,
+    className: 'bg-gray-100 text-gray-800',
+  },
+}
---
+++ data/services.tsx
@@ -0,0 +1,29 @@
+export const HOW_IT_WORKS = [
+  {
+    icon: 'UserMultiple',
+    title: 'Meet the team',
+    description: 'Hiring an agency brings additional expertise and knowledge to your business',
+  },
+  {
+    icon: 'Cursor_1',
+    title: 'Workshop together',
+    description: 'Hiring an agency brings additional expertise and knowledge to your business',
+  },
+  {
+    icon: 'Network_3Reference',
+    title: 'Regular alignement',
+    description: 'Hiring an agency brings additional expertise and knowledge to your business',
+  },
+  {
+    icon: 'CircleDash',
+    title: 'Iterations',
+    description: 'Hiring an agency brings additional expertise and knowledge to your business',
+  },
+  {
+    icon: 'Task',
+    title: 'Agile',
+    description: 'Hiring an agency brings additional expertise and knowledge to your business',
+  },
+]
+
+export default {}
