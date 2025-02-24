e8e8de76c43a05816b3ad410ac4bd6f606353ebe
Varya Stepanova <mail@varya.me>
2025-02-24 12:32:30 +0100

    new handout page

--- app/workshops/hands-on-design-systems/handout/page.tsx
+++ app/workshops/hands-on-design-systems/handout/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Handout } from '@/components-new/workshop/Handout'
+import { handoutBlocks } from '@/data/workshops/hands-on-design-systems/handout'
 
 export const metadata = {
   title: 'Handout for the workshop "Hands-on with Design Systems"',
 }
 
 export default function HandoutPage() {
-  return <Handout />
+  return <Handout blocks={handoutBlocks} />
 }
