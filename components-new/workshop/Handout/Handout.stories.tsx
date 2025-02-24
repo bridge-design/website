efbc46b3dc471ee024437e5926c2af718dd24e99
Varya Stepanova <mail@varya.me>
2025-02-24 12:23:31 +0100

    new handout component

---
+++ components-new/workshop/Handout/Handout.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Handout } from '.'
+
+const meta = {
+  title: 'Components/Workshop/Handout',
+  component: Handout,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Handout>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
