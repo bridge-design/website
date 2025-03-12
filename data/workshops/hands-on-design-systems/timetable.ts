2eea641924436321f7f148755c978d1f42c5fcb3
Varya Stepanova <mail@varya.me>
2025-03-12 14:33:18 +0100

    feat: selecting times

--- data/workshops/hands-on-design-systems/timetable.ts
+++ data/workshops/hands-on-design-systems/timetable.ts
@@ -2,52 +2,52 @@ import { TimetableItem } from '@/components-new/Timetable/Timetable'
 
 export const timetableItems: TimetableItem[] = [
   {
-    time: '9:00-9:30',
+    time: '9:00 — 9:30',
     title: 'Introduction',
     description: 'Say Hi 👋',
   },
   {
-    time: '9:30-10:00',
+    time: '9:30 — 10:00',
     title: 'Teams',
     description: 'Icebreaker exercise',
   },
   {
-    time: '10:00-10:30',
+    time: '10:00 — 10:30',
     title: 'Design and process',
     description: 'Setting up Figma project and project repository',
   },
   {
-    time: '10:30-11:30',
+    time: '10:30 — 11:30',
     title: 'Design and process',
     description: 'UI inventory and scoping',
   },
   {
-    time: '11:30-12:30',
+    time: '11:30 — 12:30',
     title: 'Designing and building the library',
     description: 'Design tokens foundation, Tokens documentation in Storybook',
   },
   {
-    time: '12:30-13:00',
+    time: '12:30 — 13:00',
     title: 'Lunch break',
     description: '',
   },
   {
-    time: '13:00-13:30',
+    time: '13:00 — 13:30',
     title: 'Designing and building the library',
     description: 'Hands-on practice',
   },
   {
-    time: '13:30-14:30',
+    time: '13:30 — 14:30',
     title: 'Designing and building the library',
     description: 'Coding and documenting the components. New design challenge',
   },
   {
-    time: '14:30-15:30',
+    time: '14:30 — 15:30',
     title: 'Design system lifecycle',
     description: 'Versioning, design update, and release',
   },
   {
-    time: '15:30-16:00',
+    time: '15:30 — 16:00',
     title: 'Recap',
     description: 'Q&A session and discussion',
   },
